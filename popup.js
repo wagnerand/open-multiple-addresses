1a128b55483162285126c119b3308e12098c0a0f
Andreas Wagner <mail@andreaswagner.org>
2018-03-18 00:51:27 +0100

    Add paste button, enable browser-style

--- popup.js
+++ popup.js
@@ -1,30 +1,48 @@
+function pasteIntoTextArea() {
+    document.addEventListener("paste", (event) => {
+        document.querySelector("#urls").value = event.clipboardData.getData("text");
+        event.preventDefault();
+    }, { once: true });
+    document.execCommand("paste");
+}
+
 function openFromTextArea() {
     let urlsElem = document.querySelector("#urls");
     let urlsArray = urlsElem.value.split("\n");
     let absUrl = null;
+    console.log(urlsArray)
     for (url of urlsArray) {
         if (url.length > 0) {
+            url = encodeURI(url);
             if (url.match(/^(https?|ftp):\/\//)) {
                 absUrl = url;
             } else {
                 absUrl = "http://" + url;
             }
-            chrome.tabs.create({url: absUrl});
+            chrome.tabs.create({ url: absUrl });
         }
     }
     window.close();
 }
 
-document.addEventListener("keypress", (event) => {
-    if ((window.navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey) && (event.key == "Enter")) {
-       openFromTextArea();
-       event.preventDefault();
-    }
-}, true);
-
 document.querySelector("#openFromTextArea").addEventListener("click", (event) => {
     openFromTextArea();
     event.preventDefault();
+}, true);
+
+document.querySelector("#pasteAndOpenFromTextArea").addEventListener("click", (event) => {
+    pasteIntoTextArea();
+    openFromTextArea();
+}, true);
+
+document.addEventListener("keypress", (event) => {
+    if ((window.navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey) && (event.key == "Enter")) {
+        if (event.shiftKey) {
+            pasteIntoTextArea();
+        }
+        openFromTextArea();
+        event.preventDefault();
+    }
 }, true);
 
 document.addEventListener("load", (event) => {
